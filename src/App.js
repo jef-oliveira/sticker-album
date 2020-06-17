fc72ad10eb65a485601bc70ef504fb599e5887e7
Jeferson de Oliveira <jef.oli@gmail.com>
2020-06-16 21:17:09 -0300

    A simple Facebook atuthetication test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import FacebookLogin from 'react-facebook-login';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [userData, setUserData] = useState();
+
+  const componentClicked = () => {
+    console.log('clicked')
+  };
+
+  const responseFacebook = (response) => {
+    console.log(response);
+    if (response && response.status != 'unknown')
+      setUserData({ 
+        id: response.userID, 
+        email: response.email, 
+        name: response.name,
+        picture: `http://graph.facebook.com/${response.userID}/picture?type=large`
+      });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +37,24 @@ function App() {
         >
           Learn React
         </a>
+
+        {!userData && (
+          <FacebookLogin
+            appId="291417558665029"
+            autoLoad={false}
+            fields="name,email,picture"
+            onClick={componentClicked}
+            callback={responseFacebook}
+          />
+        )}
+
+        {!!userData && (
+          <div onClick={() => setUserData()}>
+            <img src={userData.picture} />
+            {userData.name}
+            {userData.email}
+          </div>
+        )}
       </header>
     </div>
   );
